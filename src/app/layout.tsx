3cf1a6a2589c8fe142deda0002e5cd97a9c2ea46
nightvoyager111 <xianghanzzz@outlook.com>
2025-11-22 14:03:53 -0500

    Replaced the default icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Amelia Zhang - Portfolio",
-  description: "ECE student @ U of T.",
+  description: "ECE @ U of T.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
